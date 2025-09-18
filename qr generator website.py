b0440ca2b628ad0c5146251cb1374ca07f1d0419
tanishqbihani <87018616+tanishqbihani@users.noreply.github.com>
2025-09-18 23:08:43 +0530

    qr generator website.py

--- qr generator website.py
+++ qr generator website.py
@@ -2,8 +2,8 @@ import qrcode
 import PIL.Image
 
 # Load the image of the Python QR code
-img = PIL.Image.open('paradise.jpg')
-data = "https://drive.google.com/file/d/1nsOzttc0_qO6xHbK_oCgwsYn3kVsy1mS/view?usp=drivesdk"
+img = PIL.Image.open('maths.jpg')
+data = "https://drive.google.com/drive/folders/1_GYqfLM0dajaLwbQr_DGds9b2nWOB5_O?usp=drive_link"
 qr = qrcode.QRCode(version=1, box_size=10, border=15)
 qr.add_data(data)
 qr.make(fit=True)
@@ -11,7 +11,7 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="cyan", back_color="black")
 
 # Load the image of the logo
-logo = PIL.Image.open('player.jpg')
+logo = PIL.Image.open('maths.jpg')
 
 # Resize the logo so that it is smaller than the QR code
 logo = logo.resize((80, 80))
@@ -20,4 +20,5 @@ logo = logo.resize((80, 80))
 img.paste(logo, (300, 300))
 
 # Save the image
-img.save('gamers paradise.jpg')
+img.save('maths.jpg')
+
